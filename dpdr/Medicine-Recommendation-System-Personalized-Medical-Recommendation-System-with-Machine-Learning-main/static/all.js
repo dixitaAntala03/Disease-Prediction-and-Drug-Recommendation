c0e4fc87724628d3ff7fd96cb2b1d4bbda7bb7f8
dixitaAntala03 <145205208+dixitaAntala03@users.noreply.github.com>
2024-08-10 10:18:22 +0530

    Add files via upload

---
+++ dpdr/Medicine-Recommendation-System-Personalized-Medical-Recommendation-System-with-Machine-Learning-main/static/all.js
@@ -0,0 +1,29 @@
+let menuList = document.getElementById("menuList")
+menuList.style.maxHeight = "0px";
+
+function toggleMenu(){
+    if(menuList.style.maxHeight == "0px")
+    {
+        menuList.style.maxHeight = "300px";
+    }
+    else{
+        menuList.style.maxHeight = "0px";
+    }
+}
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+if (window.pageYOffset > sticky) {
+header.classList.add("sticky");
+} else {
+header.classList.remove("sticky");
+}
+}
+
+
+
+
+
